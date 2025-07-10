9e7eefda8df1e2eb8d118aca4965666338df9c10
Jean Sacren <sakiwit@gmail.com>
2025-07-09 21:54:34 -0600

    Build Instagram home page
    First build the home route so that when an authenticated and verified
    user visits /home, they'll see the home view.
    
    Next use artisan tool to build the home view scaffolding.
    
    $ sail artisan make:view -- home
    
       INFO  View [resources/views/home.blade.php] created successfully.
    
    Build the home view based on the view scaffolding created above.  Given
    the current layout (app.blade.php) and components (like the navigation),
    the home view should follow the standard pattern used by other pages in
    the app.  It should have default slot and header slot.
    
    Build the link to home view in Livewire navigation layout for both
    primary navigation and responsive navigation.

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome');
 
+Route::view('home', 'home')
+    ->middleware(['auth', 'verified'])
+    ->name('home');
+
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
