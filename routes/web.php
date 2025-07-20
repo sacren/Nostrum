2516195a2275f59f2c674d9df4b699ad1d63c3df
Jean Sacren <sakiwit@gmail.com>
2025-07-19 21:14:17 -0600

    Use ProfileController@index for the home route
    The use of Route::view() for static pages is simple and no controller is
    needed at all.  Here we want to show an alternative through the use of
    Route::get() and the controller.

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,11 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome');
 
-Route::view('home', 'home')
+Route::get('home', ProfileController::class.'@index')
     ->middleware(['auth', 'verified'])
     ->name('home');
 
