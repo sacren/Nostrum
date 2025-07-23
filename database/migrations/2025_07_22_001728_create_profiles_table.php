cfff763aca4a7e913f4ba5c7468e813b925db8a3
Jean Sacren <sakiwit@gmail.com>
2025-07-23 03:57:22 -0600

    Build the migration for Profile
    The use of foreignId('user_id')->constrained() creates a foreign key
    that references users table id.
    
    The use of ->onDelete('cascade') ensures when a user is deleted, their
    profile is deleted as well.
    
    All other fields are nullable, so they don't have to be filled.  But
    user_id field is required and the factory must provide a user_id.

--- database/migrations/2025_07_22_001728_create_profiles_table.php
+++ database/migrations/2025_07_22_001728_create_profiles_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('profiles', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('title')->nullable();
+            $table->text('description')->nullable();
+            $table->string('url')->nullable();
             $table->timestamps();
         });
     }
