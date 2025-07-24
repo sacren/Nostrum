6eeec4b0cbd831a83bce0d41cdf6651bcd2ab6fc
Jean Sacren <sakiwit@gmail.com>
2025-07-23 18:33:59 -0600

    Build ProfileSeeder seeder
    Use Eloquent Model Factory directly and create new Profile via the
    factory, explicitly setting user_id for clean and standard Laravel
    approach.

--- database/seeders/ProfileSeeder.php
+++ database/seeders/ProfileSeeder.php
@@ -2,7 +2,8 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Profile;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class ProfileSeeder extends Seeder
@@ -12,6 +13,8 @@ class ProfileSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        User::all()->each(function (User $user) {
+            Profile::factory()->create([ 'user_id' => $user->id ]);
+        });
     }
 }
