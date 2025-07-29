f8942a594d2eeadba6192496e50bef529e03c92c
Jean Sacren <sakiwit@gmail.com>
2025-07-28 21:16:54 -0600

    Build PostSeeder seeder
    Build the seeder to seed Post database for four users totaling 20 posts
    with image for each post.

--- database/seeders/PostSeeder.php
+++ database/seeders/PostSeeder.php
@@ -2,7 +2,8 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class PostSeeder extends Seeder
@@ -12,6 +13,16 @@ class PostSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $users = User::limit(4)->get();
+
+        if ($users->isEmpty()) {
+            $users = User::factory(4)->create();
+        }
+
+        foreach ($users as $user) {
+            Post::factory(6)->create([
+                'user_id' => $user->id,
+            ]);
+        }
     }
 }
