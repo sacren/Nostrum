bbb68357b862a5ef14a8c68687b94ec8a0015210
Jean Sacren <sakiwit@gmail.com>
2025-08-03 19:17:21 -0600

    Build route for the posts

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -15,6 +16,10 @@ Route::view('profile', 'profile')
 
 Route::resource('profiles', ProfileController::class)
     ->only(['index', 'show'])
+    ->middleware(['auth', 'verified']);
+
+Route::resource('posts', PostController::class)
+    ->only(['create', 'store'])
     ->middleware(['auth', 'verified']);
 
 require __DIR__.'/auth.php';
